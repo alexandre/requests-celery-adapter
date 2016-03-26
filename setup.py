ad5ea847a25d52e695fabc7cb8fb60ae9e2a7295
Alexandre <0x41e@protonmail.ch>
2016-03-26 00:17:29 -0300

    using boto3 when pyhton3

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+import sys
+PY3 = sys.version_info.major == 3
 
 from setuptools import setup, find_packages
 
@@ -9,7 +11,7 @@ with open(os.path.join(here, 'README.md')) as f:
 requires = ['kombu==3.0.26',
             'requests==2.3.0',
             'click==3.1',
-            'boto==2.30.0',
+            'boto==2.30.0' if not PY3 else 'boto3==1.3.0',
             ]
 
 setup(name='celery_adapter',
